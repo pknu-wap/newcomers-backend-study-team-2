96369be2f3f785ae3460af8fbc25773afe43b44a
sseung416 <choiseungyeon416@gmail.com>
2026-02-26 19:05:46 +0900

    chore: PostController url 변경

--- src/main/java/com/example/demo/controller/PostController.java
+++ src/main/java/com/example/demo/controller/PostController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/posts")
+@RequestMapping("/boards")
 public class PostController {
 
     private final PostService service;
